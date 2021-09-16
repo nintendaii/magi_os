116dddd7727c231e6b69f58ad5ea0e9d220d0fcb
Daniel <danielkarshai8@gmail.com>
2021-09-16 21:59:41 +0300

    Expanded writer to println macro

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,16 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    println!("Hello pou {}", 69);
+    panic!("Some panic message");
     loop {}
 }
 
 use core::panic::PanicInfo;
 
+/// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
