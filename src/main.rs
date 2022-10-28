e762665fc9c1dde4c18b5da1fa2a860da96d9602
Daniel <danielkarshai8@gmail.com>
2022-10-28 17:32:35 +0300

    Added memory module & Page tables support

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,46 @@ mod logo;
 mod vga_buffer;
 mod serial;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+use bootloader::{BootInfo, entry_point};
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use magi_os::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
+
     logo::show();
     //optional color setting
     vga_buffer::WRITER.lock().color_code = vga_buffer::ColorCode::new(vga_buffer::Color::White, vga_buffer::Color::Black);
     magi_os::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
+
+
     
     #[cfg(test)]
     test_main();
     println!("It did not crash!");
     magi_os::hlt_loop();
 }
-
 
 use core::panic::PanicInfo;
 
