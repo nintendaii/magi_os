54ca6bbfbb439253ca543d19fec03c0adb979f88
Daniel <danielkarshai8@gmail.com>
2022-11-11 16:23:35 +0200

    Minor main.rs fix

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use magi_os::memory;
     use magi_os::allocator;
-    use x86_64::{structures::paging::Page, VirtAddr};
+    use x86_64::{VirtAddr};
     use magi_os::memory::{BootInfoFrameAllocator};
 
 
@@ -28,9 +28,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe {
-        BootInfoFrameAllocator::init(&boot_info.memory_map)
-    };
 
     let mut frame_allocator = unsafe {
         BootInfoFrameAllocator::init(&boot_info.memory_map)
