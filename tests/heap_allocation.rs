39671ad69228a47b94ff87dc804e574c7249ea66
Daniel <danielkarshai8@gmail.com>
2022-11-20 13:43:56 +0200

    Many boxes test added

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -39,6 +39,16 @@ fn large_vec() {
     assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
 }
 
+#[test_case]
+fn many_boxes_long_lived() {
+    let long_lived = Box::new(1); // new
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+    assert_eq!(*long_lived, 1); // new
+}
+
 
 fn main(boot_info: &'static BootInfo) -> ! {
     use magi_os::allocator;
